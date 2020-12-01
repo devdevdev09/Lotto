5f877b72663f691074b6b318ba493162059f3f91
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-12-01 20:09:18 +0900

    default controller

---
+++ src/main/java/com/heo/lotto/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.heo.lotto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/lotto")
+    public String getLotto(){
+        return "lotto";
+    }
+}
