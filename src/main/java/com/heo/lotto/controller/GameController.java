a9adab9335b6567d1678704a360757ec8bd3f290
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-12-15 22:55:18 +0900

    add log file

--- src/main/java/com/heo/lotto/controller/GameController.java
+++ src/main/java/com/heo/lotto/controller/GameController.java
@@ -1,16 +1,21 @@
 package com.heo.lotto.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.heo.lotto.service.NumberService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     final int LOTTO_BALL_COUNT = 6;
 
@@ -33,6 +38,17 @@ public class GameController {
         }
 
         return list;
+    }
+
+    @RequestMapping("/makeLog")
+    public void makeLog(){
+        for(int i = 0; i < 10000; i++){
+            int[] arr = numberSerivce.getNumber(LOTTO_BALL_COUNT);
+            // for(int j = 0 ; j < LOTTO_BALL_COUNT; j++){
+            //     logger.info(i + " : " + "1,2,3,4,5");
+            // }
+            logger.info(i + " : " + Arrays.stream(arr).mapToObj(s->String.valueOf(s)).collect(Collectors.joining(",")));
+        }
     }
     
 }
