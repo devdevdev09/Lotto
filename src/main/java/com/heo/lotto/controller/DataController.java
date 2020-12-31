88ff8dd20d8f017b7b177575f28a4882bbeb29fc
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-12-31 18:53:53 +0900

    add Data controller,service

---
+++ src/main/java/com/heo/lotto/controller/DataController.java
@@ -0,0 +1,8 @@
+package com.heo.lotto.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataController {
+    
+}
---
+++ src/main/java/com/heo/lotto/service/DataService.java
@@ -0,0 +1,8 @@
+package com.heo.lotto.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+    
+}
