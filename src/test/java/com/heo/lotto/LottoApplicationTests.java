4acf848f00043202371b9032759c2d502321f828
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-01-03 22:16:12 +0900

    add test

--- src/test/java/com/heo/lotto/LottoApplicationTests.java
+++ src/test/java/com/heo/lotto/LottoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.heo.lotto;
 
+import com.heo.lotto.service.GameService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,21 @@ class LottoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	GameService gameService;
+
+	@Test
+	void checkTest(){
+		gameService = new GameService();
+		int[] target = {1,2,3,4,5,6};
+		int[] input = {1,2,3,4,5,7};
+		int bonus = 8;
+
+		int result = gameService.isWinRank(target, input, bonus);
+		if(result == 1)
+			System.out.println("test");
+
 	}
 
 }
