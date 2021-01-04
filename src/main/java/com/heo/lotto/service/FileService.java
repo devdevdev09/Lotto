d1f080c30f78388aad7936d67ece52eb235e7df9
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-01-04 22:38:02 +0900

    add file service class

---
+++ src/main/java/com/heo/lotto/service/FileService.java
@@ -0,0 +1,19 @@
+package com.heo.lotto.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+    public String getFileRead(String filePath){
+        return "";
+    }
+
+    public int[][] parseNumbers(){
+        return null;
+    }
+
+    // 9982 : [02][05][12][15][32][39]
+    public int[] getNumber(){
+        return null;
+    }
+}
