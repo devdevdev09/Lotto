2d20884ebc37cf2746f4586176fe51f3d07ee3ab
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-02-14 22:19:12 +0900

    add interface

--- src/main/java/com/heo/lotto/service/NumberService.java
+++ src/main/java/com/heo/lotto/service/NumberService.java
@@ -10,34 +10,8 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class NumberService {
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-    
-    final int MAX_COUNT = 45;
+public interface NumberService {
+    public int[] getNumber(int cnt);
 
-    public int[] getNumber(int cnt){
-        List<Integer> list = initList(MAX_COUNT);
-        List<Integer> result = new ArrayList<Integer>();
-
-        while(result.size() < cnt){
-            int num = new Random().nextInt(list.size());
-            result.add(list.get(num));
-            list.remove(list.get(num));
-        }
-
-        int[] lotto = result.stream().mapToInt(i->i).sorted().toArray();
-
-        return lotto;
-    }
-
-    public List<Integer> initList(int max){
-
-        List<Integer> list = new ArrayList<Integer>();
-
-        for(int i = 1 ; i <= max; i++){
-            list.add(i);
-        }
-
-        return list;
-    }   
+    public List<Integer> initList(int max);
 }
