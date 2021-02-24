7dcd16d804a8f3b1bc6e7a62f2ea7a649cb1756d
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-02-24 23:47:00 +0900

    add test restdocs

--- src/test/java/com/heo/lotto/controller/GameControllerTest.java
+++ src/test/java/com/heo/lotto/controller/GameControllerTest.java
@@ -1,8 +1,41 @@
 package com.heo.lotto.controller;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GameControllerTest {
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+    private MockMvc mockMvc;
+    private RestDocumentationResultHandler document;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setUp(){
+        this.document = document(
+            "{class-name}/{method-name}",
+            preprocessResponse(prettyPrint())
+        );
+    }
     
 }
