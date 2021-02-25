b000f20600dc626e7c2ca829a82e02e071d65b42
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-02-25 23:11:55 +0900

    test..

--- src/test/java/com/heo/lotto/controller/GameControllerTest.java
+++ src/test/java/com/heo/lotto/controller/GameControllerTest.java
@@ -2,10 +2,13 @@ package com.heo.lotto.controller;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,9 +16,21 @@ import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+// import static
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,10 +47,20 @@ public class GameControllerTest {
 
     @Before
     public void setUp(){
-        this.document = document(
-            "{class-name}/{method-name}",
-            preprocessResponse(prettyPrint())
-        );
+        // this.document = document(
+        //     "{class-name}/{method-name}",
+        //     preprocessResponse(prettyPrint())
+        // );
+
+        // this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+        // .apply(documentationConfiguration(this.restDocumentation)
+        // .uris().withScheme("http").withHost("127.0.0.1").withPort(2222))
+        // .alwaysDo(document)
+        // .build();
     }
-    
+
+    @Test
+    public void getLotto() throws Exception{
+    }
+       
 }
