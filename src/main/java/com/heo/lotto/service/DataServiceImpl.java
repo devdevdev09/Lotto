928d8049eee153832bd840bfcc6fb98faad01a76
dev09 <devdevdevdevdev09@gmail.com>
2021-05-08 23:11:35 +0900

    update winupdate date

--- src/main/java/com/heo/lotto/service/DataServiceImpl.java
+++ src/main/java/com/heo/lotto/service/DataServiceImpl.java
@@ -12,29 +12,31 @@ public class DataServiceImpl implements DataService {
     private ApiService apiService;
     private DateService dateService;
     private MessageService slack;
-    
+
     boolean isWinUpdate = false;
 
-    public DataServiceImpl(DateService dateService, ApiService apiService, MessageService slack){
+    public DataServiceImpl(DateService dateService, ApiService apiService, MessageService slack) {
         this.dateService = dateService;
         this.apiService = apiService;
         this.slack = slack;
     }
-    
+
     @Override
-    @Scheduled(cron="0 */10 21-23 * * 6")
-    public void getWinNumber(){
+    @Scheduled(cron = "10 */10 21-23 * * 6")
+    public void getWinNumber() {
         int weekNo = dateService.getWeekNo();
 
         Lotto result = apiService.getLottoByNo(weekNo);
-        
-        if(!result.getReturnValue().equals("fail") && !isWinUpdate){
+
+        if (!result.getReturnValue().equals("fail") && !isWinUpdate) {
             isWinUpdate = true;
             slack.sendMessage(result.toString());
         }
     }
+
     @Override
-    public void updateFalse(){
+    @Scheduled(cron = "0 0 21 * * 6")
+    public void updateFalse() {
         isWinUpdate = false;
     }
 
